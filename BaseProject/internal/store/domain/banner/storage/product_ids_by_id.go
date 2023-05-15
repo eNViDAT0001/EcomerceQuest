ffabfe84e1a8840db51954c28414af1f287b467f
eNViDAT <ngvidat@gmail.com>
2023-05-15 22:23:44 +0700

    Not in

--- BaseProject/internal/store/domain/banner/storage/product_ids_by_id.go
+++ BaseProject/internal/store/domain/banner/storage/product_ids_by_id.go
@@ -3,6 +3,7 @@ package storage
 import (
 	"context"
 	"github.com/eNViDAT0001/Thesis/Backend/external/wrap_gorm"
+	entities2 "github.com/eNViDAT0001/Thesis/Backend/internal/product/entities"
 	"github.com/eNViDAT0001/Thesis/Backend/internal/store/entities"
 )
 
@@ -32,12 +33,10 @@ func (b bannerStorage) ProductIDsByNotInBannerID(ctx context.Context, bannerID u
 	}
 	db := wrap_gorm.GetDB()
 
-	query := db.Table(entities.Banner{}.TableName()).
-		Select("DISTINCT BannerDetail.product_id").
-		Joins("JOIN BannerDetail ON BannerDetail.banner_id = Banner.id").
-		Where("Banner.id = ?", bannerID).
-		Where("BannerDetail.product_id NOT IN (?)", productIDs).
-		Where("Banner.deleted_at IS NULL")
+	query := db.Table(entities2.Product{}.TableName()).
+		Select("Product.id").
+		Where("Product.id NOT IN (?)", productIDs).
+		Where("Product.deleted_at IS NULL")
 
 	err = query.Find(&result).Error
 	if err != nil {
